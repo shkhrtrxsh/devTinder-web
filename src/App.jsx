922016c3d2d885d694dc28569125287660ba5ae5
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-03-03 00:28:05 +0530

    Added Redux for state management and wrap app with Redux Provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import NavBar from "./NavBar";
 import Body from "./Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<div>Profile</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<div>Profile</div>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
